16fcd72e10b5d933571b863e2fa5c137d910f0b6
fanuelhpa <fanuelrique@gmail.com>
2026-05-08 13:25:05 -0300

    Prepare project for cloud deployment

--- src/main/java/com/salao/salao_api/security/SecurityConfig.java
+++ src/main/java/com/salao/salao_api/security/SecurityConfig.java
@@ -19,7 +19,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig {
 
     private final JwtFilter jwtFilter;
-    
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
